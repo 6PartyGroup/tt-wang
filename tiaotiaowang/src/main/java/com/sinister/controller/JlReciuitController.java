7c0d70e0ec36be7723619e44eec93e6e63cde178
90 <wuming2573@gmail.com>
2017-05-17 18:02:01 +0800

    jj

---
+++ tiaotiaowang/src/main/java/com/sinister/controller/JlReciuitController.java
@@ -0,0 +1,36 @@
+package com.sinister.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sinister.entity.JlReciuit;
+import com.sinister.service.JlReciuitService;
+
+@Controller
+public class JlReciuitController {
+
+	@Autowired
+	private JlReciuitService jlReciuitService ;
+	
+	@RequestMapping("jltest")
+	@ResponseBody
+	public String test(){
+		return "OK";
+	}
+	
+
+	/**
+	 * JL Controller≤„±£¥Ê’–∆∏–≈œ¢
+	 * @param jlReciuit
+	 * @return
+	 */
+	@RequestMapping("JlSaveReciuit.do")
+	@ResponseBody
+	public String JlSaveReciuit(JlReciuit jlReciuit){
+		jlReciuitService.JlSaveReciuit(jlReciuit);
+		return "saveSuccess" ;
+	}
+	
+}
---
+++ tiaotiaowang/src/main/java/com/sinister/dao/JlReciuitDao.java
@@ -0,0 +1,15 @@
+package com.sinister.dao;
+
+import com.sinister.entity.JlReciuit;
+
+public interface JlReciuitDao {
+
+	/**
+	 * ±£¥Ê’–∆∏–≈œ¢
+	 * @param jlReciuit
+	 */
+	public void JlSaveReciuit(JlReciuit jlReciuit);
+	
+	
+	
+}
