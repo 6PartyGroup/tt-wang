e137036ffa256d3f2a0f34e87a8df817ed4bc711
90 <wuming2573@gmail.com>
2017-05-18 19:10:37 +0800

    aa

--- tiaotiaowang/src/main/java/com/sinister/controller/JlReciuitController.java
+++ tiaotiaowang/src/main/java/com/sinister/controller/JlReciuitController.java
@@ -1,36 +1,58 @@
 package com.sinister.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sinister.entity.AllJobs;
 import com.sinister.entity.JlReciuit;
+import com.sinister.entity.TtCom;
+import com.sinister.service.AllJobsService;
 import com.sinister.service.JlReciuitService;
 
 @Controller
 public class JlReciuitController {
 
 	@Autowired
-	private JlReciuitService jlReciuitService ;
-	
-	@RequestMapping("jltest")
+	private JlReciuitService jlReciuitService;
+
+	@Autowired
+	private AllJobsService allJobsService;
+
+	// from ankun find allJobs
+	@RequestMapping("jlFindAllJobs.do")
 	@ResponseBody
-	public String test(){
-		return "OK";
+	public List<AllJobs> findAllJobs() {
+		return allJobsService.get();
 	}
-	
 
 	/**
-	 * JL Controller锟姐保锟斤拷锟斤拷聘锟斤拷息
+	 * JL Controller层保存招聘信息
+	 * 
 	 * @param jlReciuit
 	 * @return
 	 */
 	@RequestMapping("JlSaveReciuit.do")
 	@ResponseBody
-	public String JlSaveReciuit(JlReciuit jlReciuit){
+	public String JlSaveReciuit(@RequestBody JlReciuit jlReciuit,HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		TtCom ttCom = (TtCom) session.getAttribute("Com");
+		ttCom.getC_id();
+		//准备使用公司登陆时的账号id查询公司的具体信息的id值，并将查询结果传入jlReciuit.setC_mid
+		//==========================
+		//准备书写发布简历公司的公司信息id值
+		jlReciuit.setC_mid(0);
+		
 		jlReciuitService.JlSaveReciuit(jlReciuit);
-		return "saveSuccess" ;
+		return "saveSuccess";
 	}
-	
+
 }
