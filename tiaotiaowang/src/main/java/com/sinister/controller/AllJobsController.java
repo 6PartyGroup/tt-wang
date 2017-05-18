4d1a511eb06edfa61516a28d139e31656cf08124
ak <694071397@qq.com>
2017-05-18 17:39:47 +0800

    提交

--- tiaotiaowang/src/main/java/com/sinister/controller/AllJobsController.java
+++ tiaotiaowang/src/main/java/com/sinister/controller/AllJobsController.java
@@ -1,22 +1,39 @@
 package com.sinister.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sinister.dao.AkJobsDao;
+import com.sinister.entity.AkModel;
+import com.sinister.entity.Crecruit;
+import com.sinister.service.AkJobsService;
 import com.sinister.service.AllJobsService;
+import com.sinister.service.GZUserService;
 
 @Controller
 public class AllJobsController {
 	
 	//ankun 
 	@Autowired
-	private AllJobsService allJobsService;
-	@RequestMapping("lo.do")
-	public String find(){
-		System.out.println("12");
-		allJobsService.get();
-		return "a";
+	private AkJobsService akJobsService;
+	@RequestMapping("akfind.do")
+	@ResponseBody
+	public List<AkModel> find(){
+		List<AkModel> akModels = akJobsService.selectAkJobs();
+		System.out.println("51131365");
+		return akModels;
+	}
+	@RequestMapping("akfindc.do")
+	@ResponseBody
+	public List<AkModel> findc(@RequestBody String name){
+		List<AkModel> akModels = akJobsService.selectAkJobsFromName(name);
+		System.out.println("1234");
+		return akModels;
 	}
 	@RequestMapping("a.do")
 	public String a(){
