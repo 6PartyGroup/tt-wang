84c64a914d716e87026821af3d159c2e0cda09d6
xxs <gao940681960@163.com>
2017-05-18 08:28:34 +0800

    entity

---
+++ tiaotiaowang/src/main/java/com/sinister/entity/Cmessage.java
@@ -0,0 +1,78 @@
+package com.sinister.entity;
+
+public class Cmessage {
+	
+	private int mid;
+	private String name;
+	private String telon;
+	private String address;
+	private String email;
+	private String scale;
+	private String nature;
+	private String logo;
+	private int cid;
+	private String show;
+	
+	
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTelon() {
+		return telon;
+	}
+	public void setTelon(String telon) {
+		this.telon = telon;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getScale() {
+		return scale;
+	}
+	public void setScale(String scale) {
+		this.scale = scale;
+	}
+	public String getNature() {
+		return nature;
+	}
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getShow() {
+		return show;
+	}
+	public void setShow(String show) {
+		this.show = show;
+	}
+
+}
---
+++ tiaotiaowang/src/main/java/com/sinister/entity/GZmondel.java
@@ -0,0 +1,29 @@
+package com.sinister.entity;
+
+public class GZmondel {
+	
+	private AllJobs alljobs;
+	private Cmessage cmessage;
+	private Crecruit crecruit;
+	
+	public AllJobs getAlljobs() {
+		return alljobs;
+	}
+	public void setAlljobs(AllJobs alljobs) {
+		this.alljobs = alljobs;
+	}
+	public Cmessage getCmessage() {
+		return cmessage;
+	}
+	public void setCmessage(Cmessage cmessage) {
+		this.cmessage = cmessage;
+	}
+	public Crecruit getCrecruit() {
+		return crecruit;
+	}
+	public void setCrecruit(Crecruit crecruit) {
+		this.crecruit = crecruit;
+	}
+	
+
+}
