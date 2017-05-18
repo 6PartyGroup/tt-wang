2ebc20ec01a390654493273846c876328ec95894
ak <694071397@qq.com>
2017-05-18 10:08:36 +0800

    ankunchangjian

---
+++ tiaotiaowang/src/main/java/com/sinister/controller/AkJobsController.java
@@ -0,0 +1,5 @@
+package com.sinister.controller;
+
+public class AkJobsController {
+
+}
---
+++ tiaotiaowang/src/main/java/com/sinister/entity/AkModel.java
@@ -0,0 +1,32 @@
+package com.sinister.entity;
+
+public class AkModel {
+	private AllJobs allJobs;
+	private JlReciuit jlReciuit;
+	private TtComMessageEntity comMessageEntity;
+
+	public AllJobs getAllJobs() {
+		return allJobs;
+	}
+
+	public void setAllJobs(AllJobs allJobs) {
+		this.allJobs = allJobs;
+	}
+
+	public JlReciuit getJlReciuit() {
+		return jlReciuit;
+	}
+
+	public void setJlReciuit(JlReciuit jlReciuit) {
+		this.jlReciuit = jlReciuit;
+	}
+
+	public TtComMessageEntity getComMessageEntity() {
+		return comMessageEntity;
+	}
+
+	public void setComMessageEntity(TtComMessageEntity comMessageEntity) {
+		this.comMessageEntity = comMessageEntity;
+	}
+
+}
