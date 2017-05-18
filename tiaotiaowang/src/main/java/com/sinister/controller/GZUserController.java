a5729f471ebcdc958d924a3d36938d5d02f3d388
xxs <gao940681960@163.com>
2017-05-18 11:03:06 +0800

    最新版5.18.10.57－－－－gz

--- tiaotiaowang/src/main/java/com/sinister/controller/GZUserController.java
+++ tiaotiaowang/src/main/java/com/sinister/controller/GZUserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sinister.dao.GZUserDao;
 import com.sinister.entity.Crecruit;
 import com.sinister.entity.GZUserEntity;
 import com.sinister.service.GZUserService;
@@ -16,14 +17,25 @@ public class GZUserController {
 	@Autowired
 	private GZUserService gzuserservice;
 	
+	
 	@RequestMapping("find.do")
 	@ResponseBody
 	public String findUser(@RequestBody GZUserEntity gz){
 		
-		
-		Crecruit crecruit =gzuserservice.findGZjob("载驰科技");
+		//查找招聘信息表
+		Crecruit crecruit =gzuserservice.findcrecruit(1);
 		System.out.println(crecruit.getAddress());
-		System.out.println(crecruit.getCmessage().getName());
+		
+		//根据公司信息表是次表  招聘信息是主表 查找招聘信息    两张表查询
+		Crecruit crecruit1 = gzuserservice.findGZjob("载弛科技");
+		System.out.println(crecruit1.getDuty());
+		System.out.println(crecruit1.getCmessage().getName());
+		
+		//三表联查
+		Crecruit crecruit2 = gzuserservice.findjcr("移动端");
+		System.out.println(crecruit2.getDuty());
+		System.out.println(crecruit2.getCmessage().getName());
+        System.out.println(crecruit2.getAlljobs().getaName());
 		
 		GZUserEntity entity = gzuserservice.findGZUser(gz.getMid());
 		System.out.println(entity.getAddress());
--- tiaotiaowang/src/main/java/com/sinister/dao/GZUserDao.java
+++ tiaotiaowang/src/main/java/com/sinister/dao/GZUserDao.java
@@ -8,9 +8,12 @@ public interface GZUserDao {
     //查找公司信息
 	public GZUserEntity findGZUser(int id);
 	
-	//根据公司查找招聘信息
+	//根据公司信息表是次表  招聘信息是主表 查找招聘信息    两张表查询
 	public Crecruit findGZjob(String company);
+
+	//三表联查
+	public Crecruit findjcr(String string);
 	
-	
-	
+	//查找招聘信息表
+	public Crecruit findcrecruit(int id);
 }
