b82839626fb0c3da23799ab129628157c7321db1
90 <wuming2573@gmail.com>
2017-05-18 09:04:43 +0800

    90-添加Impl注解

--- tiaotiaowang/src/main/java/com/sinister/service/impl/JlReciuitServiceImpl.java
+++ tiaotiaowang/src/main/java/com/sinister/service/impl/JlReciuitServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sinister.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sinister.dao.JlReciuitDao;
 import com.sinister.entity.JlReciuit;
 import com.sinister.service.JlReciuitService;
 
+@Service
 public class JlReciuitServiceImpl implements JlReciuitService{
 
 	@Autowired
